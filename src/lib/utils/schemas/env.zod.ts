e6378e714d277e2c9ff6de964c2f61cf787f73d2
mmazitov <mazitov.nikolya@gmail.com>
2025-05-11 19:36:39 +0300

    feat: add FATSECRET_ID and FATSECRET_SECRET to environment schema for enhanced configuration

--- src/lib/utils/schemas/env.zod.ts
+++ src/lib/utils/schemas/env.zod.ts
@@ -8,6 +8,8 @@ const envSchema = z.object({
 	NEXTAUTH_SECRET: z.string().nonempty(),
 	GOOGLE_API_KEY: z.string().nonempty(),
 	GOOGLE_CSE_ID: z.string().nonempty(),
+	FATSECRET_ID: z.string().nonempty(),
+	FATSECRET_SECRET: z.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
