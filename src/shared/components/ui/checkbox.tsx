ed80a66096ce9c9b870b800d9eb6e8c941e60406
mmazitov <mazitov.nikolya@gmail.com>
2026-02-22 19:00:53 +0200

    refactor: Convert Checkbox component to an arrow function, remove direct `onCheckedChange` prop forwarding, and reorder CSS classes.

--- src/shared/components/ui/checkbox.tsx
+++ src/shared/components/ui/checkbox.tsx
@@ -2,34 +2,34 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import * as React from 'react';
 import { LuCheck } from 'react-icons/lu';
+
 import { Label } from './label';
 
-interface Props
-	extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+interface Props extends React.ComponentPropsWithoutRef<
+	typeof CheckboxPrimitive.Root
+> {
 	label?: string;
 	subLabel?: string;
-	setChecked?: (checked: boolean) => void;
 }
 
-function Checkbox({ label, subLabel, className, setChecked, ...props }: Props) {
+const Checkbox = ({ label, subLabel, className, ...props }: Props) => {
 	return (
-		<label className="flex items-center gap-2 justify-between cursor-pointer select-none">
+		<label className="flex cursor-pointer items-center justify-between gap-2 select-none">
 			<div className="flex flex-col">
 				{label && <Label>{label}</Label>}{' '}
 				{subLabel && (
-					<p className="text-sm text-muted-foreground">{subLabel}</p>
+					<p className="text-muted-foreground text-sm">{subLabel}</p>
 				)}
 			</div>
 			<CheckboxPrimitive.Root
 				className={clsx(
-					'peer h-5 min-w-5 border border-input bg-background',
+					'peer border-input bg-background h-5 min-w-5 border',
 					'data-[state=checked]:bg-ring data-[state=checked]:border-ring',
 					'data-[state=unchecked]:bg-background',
 					'transition-colors',
 					className,
 				)}
 				{...props}
-				onCheckedChange={setChecked}
 			>
 				<CheckboxPrimitive.Indicator className="flex items-center justify-center text-white">
 					<LuCheck size={16} strokeWidth={3} />
@@ -37,6 +37,6 @@ function Checkbox({ label, subLabel, className, setChecked, ...props }: Props) {
 			</CheckboxPrimitive.Root>
 		</label>
 	);
-}
+};
 
 export { Checkbox };
