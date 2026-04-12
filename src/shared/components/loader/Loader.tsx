05b4c3fbd17a31d594640396ef8df54636417a93
mmazitov <mazitov.nikolya@gmail.com>
2026-04-12 22:35:27 +0300

    refactor: replace Circles loader with Puff spinner and increase default size
    Replace Circles spinner component with Puff spinner from react-loader-spinner. Increase default size from 80 to 100 pixels for better visibility.

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -1,15 +1,15 @@
-import { Circles } from 'react-loader-spinner';
+import { Puff } from 'react-loader-spinner';
 
 interface LoaderProps {
 	fullScreen?: boolean;
 	size?: string;
 }
 
-export const Loader = ({ fullScreen = true, size = '80' }: LoaderProps) => {
+export const Loader = ({ fullScreen = true, size = '100' }: LoaderProps) => {
 	if (fullScreen) {
 		return (
 			<div className="absolute inset-0 z-10 flex h-full items-center justify-center">
-				<Circles
+				<Puff
 					height={size}
 					width={size}
 					color="hsl(175 60% 50%)"
@@ -24,7 +24,7 @@ export const Loader = ({ fullScreen = true, size = '80' }: LoaderProps) => {
 
 	return (
 		<div className="flex items-center justify-center p-8">
-			<Circles
+			<Puff
 				height={size}
 				width={size}
 				color="hsl(175 60% 50%)"
